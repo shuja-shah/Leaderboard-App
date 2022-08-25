7949acfce78c42a16c0a94e8edf4b8fe57e40f05
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-25 12:21:31 -0700

    Develop Refreash

--- src/refreash.js
+++ src/refreash.js
@@ -1,4 +1,10 @@
-function refreash() {
-  console.log('I was Hit');
+async function refreash() {
+  const req = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/zblSE0KFde5ujWoC9BWc/scores/')
+  const response = await req.json();
+  if(response.length > 0) {
+    console.log(response);
+  } else {
+    console.log('no data');
+  }
 }
 module.exports = refreash;
