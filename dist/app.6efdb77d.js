d378113c1286b3ac5ba7bc7399a9356e509c5902
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-08-27 05:23:25 -0700

    Add Particles.js

---
+++ dist/app.6efdb77d.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/app.js":
+/*!********************!*\
+  !*** ./src/app.js ***!
+  \********************/
+/***/ (() => {
+
+eval("/*eslint-disable */\r\nparticlesJS(\"particles-js\", {\r\n  particles: {\r\n    number: {\r\n      value: 200,\r\n      density: {\r\n        enable: true,\r\n        value_area: 5000,\r\n      },\r\n    },\r\n    color: {\r\n      value: \"#feede1\",\r\n    },\r\n    shape: {\r\n      type: \"circle\",\r\n      stroke: {\r\n        width: 0,\r\n        color: \"#feede1\",\r\n      },\r\n      polygon: {\r\n        nb_sides: 5,\r\n      },\r\n      image: {\r\n        src: \"img/github.svg\",\r\n        width: 100,\r\n        height: 100,\r\n      },\r\n    },\r\n    opacity: {\r\n      value: 0.9,\r\n      random: true,\r\n      anim: {\r\n        enable: true,\r\n        speed: 0.9,\r\n        opacity_min: 0,\r\n        sync: false,\r\n      },\r\n    },\r\n    size: {\r\n      value: 6,\r\n      random: true,\r\n      anim: {\r\n        enable: false,\r\n        speed: 3,\r\n        size_min: 0.3,\r\n        sync: false,\r\n      },\r\n    },\r\n    line_linked: {\r\n      enable: false,\r\n      distance: 150,\r\n      color: \"#ffffff\",\r\n      opacity: 0.4,\r\n      width: 1,\r\n    },\r\n    move: {\r\n      enable: true,\r\n      speed: 4,\r\n      direction: \"none\",\r\n      random: true,\r\n      straight: false,\r\n      out_mode: \"out\",\r\n      bounce: false,\r\n      attract: {\r\n        enable: false,\r\n        rotateX: 600,\r\n        rotateY: 600,\r\n      },\r\n    },\r\n  },\r\n  interactivity: {\r\n    detect_on: \"canvas\",\r\n    events: {\r\n      onhover: {\r\n        enable: false,\r\n        mode: \"bubble\",\r\n      },\r\n      onclick: {\r\n        enable: false,\r\n        mode: \"repulse\",\r\n      },\r\n      resize: true,\r\n    },\r\n    modes: {\r\n      grab: {\r\n        distance: 400,\r\n        line_linked: {\r\n          opacity: 1,\r\n        },\r\n      },\r\n      bubble: {\r\n        distance: 250,\r\n        size: 0,\r\n        duration: 2,\r\n        opacity: 0,\r\n        speed: 3,\r\n      },\r\n      repulse: {\r\n        distance: 400,\r\n        duration: 0.4,\r\n      },\r\n      push: {\r\n        particles_nb: 4,\r\n      },\r\n      remove: {\r\n        particles_nb: 2,\r\n      },\r\n    },\r\n  },\r\n  retina_detect: true,\r\n});\r\n\n\n//# sourceURL=webpack://Leaderboard-App/./src/app.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/app.js"]();
+/******/ 	
+/******/ })()
+;
